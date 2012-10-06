c30c99c3ee9f1a43d75d360f4319d1fd06bd24bf
daveruiz <daveruiz.project@gmail.com>
2012-10-06 22:06:48 +0200

    Less restrictive linear movement detection. v0.1 alpha rev6

--- js/gestojs.js
+++ js/gestojs.js
@@ -1083,7 +1083,7 @@
 		threshold = parseFloat( threshold ) / 180 * Math.PI || Math.PI / 6 /* 30º */
 
 		return Math.abs( this.rotation ) < Math.PI / 6 /* 30º */	// not curve
-			&& this.length > 100									// min length
+			&& this.length > 5								// min length
 			&& (this.endAngle >= angle-threshold && this.endAngle <= angle+threshold
 				|| this.endAngle+(Math.PI*2) >= angle-threshold && this.endAngle+(Math.PI*2) <= angle+threshold)
 			 ? 1 - Math.abs( angle - this.endAngle ) / (threshold * 2) : 0
