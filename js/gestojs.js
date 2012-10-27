0a4ace356ea67c1656de7bf9a81495f6c477a3bc
daveruiz <daveruiz.project@gmail.com>
2012-10-27 18:48:07 +0200

    v0.1 alpha rev7

--- js/gestojs.js
+++ js/gestojs.js
@@ -12,7 +12,12 @@
 	 * @return {boolean}
 	 */
 	Utils.isTouchableDevice = function() {
-		return !!('ontouchstart' in window);
+		// Patch for lastest desktop Chrome. It throws false positive
+		if ( navigator.userAgent.indexOf("Chrome")>-1 &&
+			 !( navigator.userAgent.indexOf("iOS")>-1 || navigator.userAgent.indexOf("Android")>-1 )) return false
+
+		return ('ontouchstart' in window) ||
+			   (window.DocumentTouch && document instanceof DocumentTouch)
 	}
 
 	/**
@@ -1094,7 +1099,7 @@
 
 (function (GestoJS) {
 
-	/**
+	/*
 	 * Some simple documentation:
 	 *
 	 * Gestures are divided in time steps:
